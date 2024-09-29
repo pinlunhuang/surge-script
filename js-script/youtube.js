9aef937edeca0464133e4e8261f983c61f32271a
pinlunhuang <lunwaac@gmail.com>
2024-09-29 23:01:44 +0800

    Added youtube.js

---
+++ js-script/youtube.js
@@ -0,0 +1,9 @@
+# > YouTube
+DOMAIN-SUFFIX,youtube.com
+DOMAIN-SUFFIX,ytimg.com
+DOMAIN-SUFFIX,googlevideo.com
+DOMAIN-SUFFIX,yt3.ggpht.com
+DOMAIN-SUFFIX,youtubei.googleapis.com
+DOMAIN-SUFFIX,yt3.ggpht.com
+DOMAIN-SUFFIX,youtube-ui.l.google.com
+DOMAIN-SUFFIX,youtube-nocookie.com
